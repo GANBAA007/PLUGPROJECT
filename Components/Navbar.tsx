f0ed9ad4e905fed7d19d6691965f33b572019267
GANBAA <invisinvin87@gmail.com>
2024-06-26 14:53:05 +0800

    Merged histories

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,38 +1,48 @@
 import React from "react";
 import Link from "next/link";
+import rsz_1logoblack from '../public/rsz_1logoblack.png';
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="sticky z-20 flex bg-white text-black w-full justify-between text-xs font-semibold">
-      <div className="flex-1 align-middle">
-        <ul className="flex p-4 ">
+  <div>
+    <div className="sticky mx-auto z-20 bg-white flex text-black  justify-between  text-xs font-semibold">
+      <div>
+            <Link href="/home" className="">
+            <Image src={rsz_1logoblack} alt="logo"></Image>
+            </Link>
+          </div>
+      <div className="flex-1 py-4 align-middle ">
+
+        <ul className="flex my-1">
           <li>
-            <Link href="/hoodie" className="px-4 hover:font-bold">
+            <Link href="/hoodie" className="px-4 hover:font-bold align-middle">
               HOODDIE
             </Link>
           </li>
           <li>
-            <Link href="/tees" className="px-4 hover:font-bold">
+            <Link href="/tees" className="px-4 py-auto hover:font-bold align-middle">
               TEES
             </Link>
           </li>
           <li>
-            <Link href="/bags" className="px-4 hover:font-bold">
+            <Link href="/bags" className="px-4 hover:font-bold align-middle">
               BAGS
             </Link>
           </li>
         </ul>
       </div>
-      <div className="p-4">
-        <ul className="flex justify-end">
+      <div className="py-4">
+        <ul className="flex justify-end my-1">
           <li>
-            <Link href="/bags" className="px-4 hover:font-bold">
+            <Link href="/bags" className="px-4 hover:font-bold align-middle">
               CART
             </Link>
           </li>
         </ul>
       </div>
-      <div className="h-[1px] max-w-full bg-black"></div>
+    </div>
+    <div className="h-[1px] w-full mx-auto bg-stone-900 mb-12"></div>
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,10 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="bg-white">
+    <html lang="en" className="bg-white ">
       <body>
         <Navbar />
-        <main className="relative overflow-hidden">{children}</main>
+        <main className=" overflow-hidden">{children}</main>
         <Footer />
       </body>
     </html>
