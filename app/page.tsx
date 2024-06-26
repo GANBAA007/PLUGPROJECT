ee4187b6b2ed52f3306fce9dada0104bf921f1d4
GANBAA <invisinvin87@gmail.com>
2024-06-26 14:54:24 +0800

    Merged histories

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import React from "react";
+import plug1 from '../public/plug1.png';
+import Image from 'next/image';
+
+
+
+
 export default function Home() {
-  return <h1>aaaa</h1>;
+  return (
+    <div>
+    <div className="mx-auto grid grid-cols-3 justify-items-center gap-4 max-md:grid-cols-2 max-sm:grid-cols-1" >
+      <div><Image src={plug1} alt="Plug Image" /></div>
+      <div><Image src={plug1} alt="Plug Image" /></div>
+      <div><Image src={plug1} alt="Plug Image" /></div>
+      <div><Image src={plug1} alt="Plug Image" /></div>
+      <div><Image src={plug1} alt="Plug Image" /></div>
+      <div><Image src={plug1} alt="Plug Image" /></div>
+    </div>
+    </div>
+  );
 }
